b6254fc5c4d2db859ee7b2bad8668753dace7cce
Quockm <mjnhquoc21101997@gmail.com>
2020-08-28 18:11:38 +0700

    code feature a

--- app/src/main/java/com/example/uberclone/Common.java
+++ app/src/main/java/com/example/uberclone/Common.java
@@ -1,20 +1,21 @@
 package com.example.uberclone;
 
 public class Common {
-    public static final String DRIVER_INFO_REFERENCE = "DriverInfo" ;
+    public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
     public static final String DRIVER_LOCATION_REFERENCE = "DriverLocation";
 
     public static DriverInfoModel currentUser;
 
     public static int buildWelcomeMessage() {
-        if(Common.currentUser !=null)
-        {
-            return new StringBuilder("Welcome ")
-                    .append(Common.currentUser.getFisrtnasme())
-                    .append(" ")
-                    .append(Common.currentUser.getLastname()).toString();
-        }
-        else
-            return "";
+//        if(Common.currentUser !=null)
+//        {
+//            return new StringBuilder("Welcome ")
+//                    .append(Common.currentUser.getFisrtnasme())
+//                    .append(" ")
+//                    .append(Common.currentUser.getLastname()).toString();
+//        }
+//        else
+//            return "";
+        return  0;
     }
 }
